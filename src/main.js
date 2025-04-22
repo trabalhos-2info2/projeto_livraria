5396349140a760014626c0d2df8f5ece2740a63d
AmandaRibeiroLuz <amanda.ribeiro.da.luz.201@gmail.com>
2025-04-22 16:45:55 -0300

    FEAT: Layout dos Lançamentos finalizado. #5

--- src/main.js
+++ src/main.js
@@ -3,4 +3,7 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 
+import '@fortawesome/fontawesome-free/css/all.css'
+import '@fortawesome/fontawesome-free/js/all.js'
+
 createApp(App).mount('#app')
